e0ec1de2d7fa2d75df5518b427f39a575ebf79b9
xiaoeric <alpha2275@gmail.com>
2017-12-06 10:08:57 -0800

    Newly-born leafs crawl at 1 speed

--- ILoveMyLeaf/src/DriveCar.java
+++ ILoveMyLeaf/src/DriveCar.java
@@ -2,7 +2,7 @@
 public class DriveCar {
 
 	public static void main(String[] args) {
-		Car leaf = new Car("red", "Nissan Leaf", "REE");
+		Car leaf = new Car("red", "Nissan Leaf", "REE", 1);
 		leaf.drive();
 	}
 
